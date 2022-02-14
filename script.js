29e0d16eb04d0a5c8317bf55ce8dc6fa39a875e7
Nikita <ndemidovn@gmail.com>
2022-02-14 22:36:46 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+let number = 5;
+const leftBorder = 1;
+
+// const answers = [];
+// answers[0] = prompt('Как Ваше имя?', '');
+// answers[1] = prompt('Почему ты нихуя не делаешь?', '');
+// answers[2] = prompt('Скольто ты будешь шланговать?', '');
+
+// document.write(answers);
