abb0c5f18236315852356a233f26968f0e1c1f4e
Nikita <ndemidovn@gmail.com>
2022-02-24 02:02:39 +0300

    object

--- script.js
+++ script.js
@@ -1,25 +1,37 @@
 'use strict';
 
-function first (){
-  // Do something
-  setTimeout(function(){
-    console.log(1);
-  }, 500);
-} 
+const options = {
+  name: 'test',
+  width: 1024,
+  height: 1024,
+  colors: {
+    border: 'black',
+    bg: 'red',
+  },
+  makeTest: function(){
+    console.log('Test');
+  }
+};
+options.makeTest();
 
-function second(){
-  console.log(2);
+const {border, bg} = options.colors;
+console.log(border);
+// console.log(Object.keys(options).length);
+// console.log(options.name);
+// delete options.name;
+// console.log(options);
+
+let counter = 0;
+
+for (let key in options){
+  if (typeof(options[key]) === "object"){
+    for (let i in options[key]){
+      console.log(`Свойство ${i} имеет значение ${options[key][i]} `);
+      counter++;
+    }
+  } else{
+    console.log(`Свойство ${key} имеет значение ${options[key]} `);
+    counter++;
+  } 
 }
-
-first ();
-second();
-
-function learnJS(lang, callback){
-  console.log(`Я учусь: ${lang}`);
-  callback();
-}
-function done(){
-  console.log('Я прошел этот урок!');
-}
-
-learnJS('JavaScript', done);
+console.log(counter);
