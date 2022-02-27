668100b068401619e907a51adff742bf4b57058a
Nikita <ndemidovn@gmail.com>
2022-02-28 01:17:21 +0300

    arrew

--- script.js
+++ script.js
@@ -1,37 +1,34 @@
 'use strict';
 
-const options = {
-  name: 'test',
-  width: 1024,
-  height: 1024,
-  colors: {
-    border: 'black',
-    bg: 'red',
-  },
-  makeTest: function(){
-    console.log('Test');
-  }
-};
-options.makeTest();
+const arr = [1, 12, 13, 6, 8, 99, 1];
 
-const {border, bg} = options.colors;
-console.log(border);
-// console.log(Object.keys(options).length);
-// console.log(options.name);
-// delete options.name;
-// console.log(options);
+arr.sort(compereNum);
+console.log(arr);
 
-let counter = 0;
-
-for (let key in options){
-  if (typeof(options[key]) === "object"){
-    for (let i in options[key]){
-      console.log(`Свойство ${i} имеет значение ${options[key][i]} `);
-      counter++;
-    }
-  } else{
-    console.log(`Свойство ${key} имеет значение ${options[key]} `);
-    counter++;
-  } 
+function compereNum(a, b){
+  return a - b;
 }
-console.log(counter);
+// arr[99] = 0;
+// console.log(arr.length);
+// console.log(arr);
+
+// arr.forEach(function(item, i, arr){
+//   console.log(`${i}: ${item} внутри массива ${arr}`);
+// });
+
+// arr.pop();
+// arr.push(10);
+// console.log(arr);
+
+// for (let i = 0; i < arr.length; i++){
+//   console.log(arr[i]);
+// }
+
+// for (let value of arr){
+//   console.log(value);
+// }
+
+// const str = prompt('', '');
+// const product = str.split(', ');
+// product.sort();
+// console.log(product.join('; '));
