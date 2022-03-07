d698b7253ebfc9233dd406b5217beadbe62d4461
Nikita <ndemidovn@gmail.com>
2022-03-08 00:19:19 +0300

    Getting elements from a page

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
 'use strict';
+const box = document.getElementById('box');
+console.log(box);
+const btns = document.getElementsByTagName('button');
+console.log(btns[0]);
+const circles = document.getElementsByClassName('circle');
+console.log(circles);
 
-// let x = 5; alert(x++); - 5
-// [ ] + false - null + true; - NaN
-// let y = 1; let x = y = 2; alert(x); - 2
-// [] + 1 +2; - 12
-// alert('1'[0]); - 1
-// console.log(2 && 1 && null && 0 && undefined ); 
-// && запинается на лжи!!!
-// || запинается на правде!!!
-// console.log(!!( 1 && 2 ) === (1 && 2));
-// alert( null || 2 && 3 || 4 ); 
-// const a = [1, 2, 3]; 
-// const b = [1, 2, 3]; 
-// console.log(a == b);
-// alert( +"Infinity" );
-// console.log("Ёжик" > "яблоко"); 
-// console.log(0 || "" || 2 || undefined || true || falsе );
+const hearts = document.querySelectorAll('.heart');
+console.log(hearts);
+
+hearts.forEach(item => {
+  console.log(item);
+});
+
+const oneHeart = document.querySelector('.heart');
+console.log(oneHeart);
