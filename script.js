15ff87a9b55b4298f702b3139463fb495ece3c4a
Nikita <ndemidovn@gmail.com>
2022-03-08 23:04:09 +0300

    Actions with elements on the page

--- script.js
+++ script.js
@@ -1,17 +1,46 @@
 'use strict';
 const box = document.getElementById('box');
-console.log(box);
 const btns = document.getElementsByTagName('button');
-console.log(btns[0]);
 const circles = document.getElementsByClassName('circle');
-console.log(circles);
-
 const hearts = document.querySelectorAll('.heart');
-console.log(hearts);
+const oneHeart = document.querySelector('.heart');
+const wrapper = document.querySelector('.wrapper');
+
+// box.style.backgroundColor = 'blue';
+// box.style.width = '500px';
+
+box.style.cssText = 'background-color: blue; width: 500px';
+
+btns[1].style.borderRadius = '100%';
+circles[0].style.backgroundColor = 'red';
+
+// for (let i = 0; i < hearts.length; i++){
+//   hearts[i].style.backgroundColor = 'blue';
+// }
 
 hearts.forEach(item => {
-  console.log(item);
+  item.style.backgroundColor = 'blue';
 });
 
-const oneHeart = document.querySelector('.heart');
-console.log(oneHeart);
+const div = document.createElement('div');
+// const text = document.createTextNode('Тут был я');
+div.classList.add('black');
+
+// document.body.append(div);
+wrapper.append(div);
+// wrapper.appendChild(div);
+
+// wrapper.prepend(div);
+// hearts[0].before(div);
+// hearts[0].after(div);
+// wrapper.insertBefore(div, hearts[0]);
+
+// circles[0].remove();
+// wrapper.removeChild(hearts[1]);
+
+// hearts[0].replaceWith(circles[0]);
+// wrapper.replaceChild(circles[0], hearts[0]);
+
+div.innerHTML = '<h1>Hello World!</h1>';
+// div.textContent = 'Hello';
+div.insertAdjacentHTML('afterend', '<h2>Hello</h2>');
