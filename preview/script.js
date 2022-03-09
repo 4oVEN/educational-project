0a29936420e3a9b5c7d62b746197ff3fc5a5fbd4
Nikita <ndemidovn@gmail.com>
2022-03-10 00:04:59 +0300

    Events and their handlers

---
+++ preview/script.js
@@ -0,0 +1,33 @@
+
+const btns = document.querySelectorAll('button');
+const overlay = document.querySelector('.overlay');
+
+// btn.onclick = function(){
+//   alert('Click');
+// };
+
+// let i = 0;
+
+const deleteElement = (e) => {
+  console.log(e.currentTarget);
+  console.log(e.type);
+  // i++;
+  // if (i == 1){
+  //   btn.removeEventListener('click', deleteElement);
+  // }
+};
+
+// btn.addEventListener('click', deleteElement);
+// overlay.addEventListener('click', deleteElement);
+
+btns.forEach(btn => {
+  btn.addEventListener('click', deleteElement, {once: true});
+});
+
+const link = document.querySelector('a');
+
+link.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  console.log(event.target);
+});
