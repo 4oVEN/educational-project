2149fb11c6e4d791e3951c2858c093a8c733b6cf
Nikita <ndemidovn@gmail.com>
2022-03-11 17:01:39 +0300

    Events on mobile devices

---
+++ script.js
@@ -0,0 +1,32 @@
+// touchstart
+// touchmove
+// touchend
+// touchenter
+// touchleave
+// touchcancel
+
+window.addEventListener('DOMContentLoaded', () => {
+  const box = document.querySelector('.box');
+  box.addEventListener('touchstart', (e) => {
+    e.preventDefault();
+
+    console.log('Start');
+    console.log(e.targetTouches);
+  });
+
+  box.addEventListener('touchmove', (e) => {
+    e.preventDefault();
+
+    console.log(e.targetTouches[0].pageX);
+  });
+
+  box.addEventListener('touchend', (e) => {
+    e.preventDefault();
+
+    console.log('End');
+  });
+});
+
+// touches
+// targetTouches
+// changedTouches
