657cdb9e659414beedc38a39011b869b1a4188fb
Nikita <ndemidovn@gmail.com>
2022-03-13 19:50:47 +0300

    Date

--- script.js
+++ script.js
@@ -1,35 +1,27 @@
-const btn = document.querySelector('.btn');
-let timerId,
-    i =0;
+'use strict';
+const now = new Date('2022-13-03');
+// new Date.parse('2022-13-03');
 
- function myAnimation (){
-   const elem = document.querySelector('.box');
-   let pos = 0;
+console.log(now.setHours(40));
+console.log(now);
 
-   const id = setInterval(frame, 10);
 
-   function frame(){
-     if (pos == 300){
-       clearInterval(id);
-     }else {
-       pos++;
-       elem.style.top = pos + 'px';
-       elem.style.left = pos + 'px';
-     }
-   }
- }   
+// console.log(now.getFullYear());
+// console.log(now.getMonth());
+// console.log(now.getDate());
+// console.log(now.getDay());
+// console.log(now.getHours());
+// console.log(now.getUTCHours());
 
-btn.addEventListener('click', myAnimation);
+// console.log(now.getTimezoneOffset());
+// console.log(now.getTime());
 
-// function logger () {
-//   if (i === 3){
-//     clearInterval(timerId);
-//   }
-//   console.log('text');
-//   i++;
-// }
+let start = new Date();
 
-// let id = setTimeout(function log(){
-//   console.log('Hello');
-//   id = setTimeout(log, 500);
-// }, 500);
+for (let i = 0; i < 100000; i++){
+    let some = i ** 3;
+}
+
+let and = new Date();
+
+// alert(`Цикл отработал за ${and - start} миллисекунд`);
