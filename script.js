f90a53db9acae49289a2bf1ce96ca1634dfdd691
Nikita <ndemidovn@gmail.com>
2022-03-16 12:01:20 +0300

    Rest

--- script.js
+++ script.js
@@ -1,35 +1,12 @@
 'use strict';
 
-class Rectangle {
-  constructor(height, width){
-    this.height = height;
-    this.width = width;
-  }
+const log = function(a, b, ...rest){
+  console.log(a, b, rest);
+};
 
-  calcArea (){
-    return this.height * this.width;
-  }
+log('basic', 'rest', 'operator', 'usage');
+
+function calcOrDouble(number, basis = 2){
+  console.log(number * basis);
 }
-
-class ColoredRectangleWigthText extends Rectangle {
-  constructor (height, width, text, bgColor){
-    super(height, width);
-    this.text = text;
-    this.bgColor = bgColor;
-  }
-  showMyProps(){
-    console.log(`Текст: ${this.text}, цвет: ${this.bgColor}`);
-  }
-}
-
-const div = new ColoredRectangleWigthText(25, 10, 'Hello', 'red');
-
-div.showMyProps();
-console.log(div.calcArea());
-
-// const square = new Rectangle(10, 10);
-// const long = new Rectangle(20, 100);
-
-
-// console.log(long.calcArea());
-// console.log(square.calcArea());
+calcOrDouble(3);
