5c832b3631d2385a670dc03a1f52199a59a1abaa
Nikita <ndemidovn@gmail.com>
2022-03-23 22:48:58 +0300

    get/set

--- js/script.js
+++ js/script.js
@@ -1,31 +1,17 @@
 'use strict';
 
-// new RegExp('pattern', 'flags');
-// /pattern/flags
+const persone = {
+  name: 'Nikita',
+  age: 33,
 
-// const ans = prompt('Введите ваше число');
+  get userAge(){
+    return this.age;
+  },
 
-// const red = /\d/g;
-// console.log(ans.match(red));
+  set userAge(num){
+    this.age = num;
+  }
+};
 
-const str = 'My name is R2D2';
-
-console.log(str.match(/\D/ig));
-
-// \d
-// \w
-// \s
-
-// i
-// g
-// m
-
-// console.log(ans.search(red));
-// console.log(ans.match(red));
-
-// const pass = prompt('Password');
-
-// console.log(pass.replace(/\./g, '*'));
-
-console.log('12-34-56'.replace(/-/g, ':'));
-
+console.log(persone.userAge = 30);
+console.log(persone.userAge);
