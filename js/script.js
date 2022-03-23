f0ee7d2b3f03847ce0cb670a8148ac9c86e75c4c
Nikita <ndemidovn@gmail.com>
2022-03-23 23:49:49 +0300

    encapsulation

--- js/script.js
+++ js/script.js
@@ -1,17 +1,34 @@
 'use strict';
 
-const persone = {
-  name: 'Nikita',
-  age: 33,
-
-  get userAge(){
-    return this.age;
-  },
-
-  set userAge(num){
-    this.age = num;
+class User{
+  constructor(name, age){
+    this.name = name;
+      this._age = age;
   }
-};
+  
+  #surname = 'Demidov';
 
-console.log(persone.userAge = 30);
-console.log(persone.userAge);
+  say = () => {
+    console.log(`Имя пользователя: ${this.name}${this.#surname}, возвраст ${this._age}`);
+  }
+
+  get age (){
+    return this._age;
+  }
+
+  set age (age){
+    if (typeof  age === 'number' && age > 0 && age < 110){
+      this._age = age;
+    } else {
+      console.log('Недопустимые значения');
+    }
+  }
+}
+
+const nikita = new User('nikita', 33);
+console.log(nikita._age);
+nikita._age = 99;
+console.log(nikita._age);
+console.log(nikita.surname);
+
+nikita.say();
